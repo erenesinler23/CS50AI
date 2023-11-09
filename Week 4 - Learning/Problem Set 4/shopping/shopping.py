5408219f0e20657b4714abf4ad30e5af1124d194
Lütfi Eren Esinler <erenesinler23@gmail.com>
2023-11-10 02:15:35 +0300

    Add files via upload

---
+++ Week 4 - Learning/Problem Set 4/shopping/shopping.py
@@ -0,0 +1,163 @@
+import csv
+import sys
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+TEST_SIZE = 0.4
+
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python shopping.py data")
+
+    # Load data from spreadsheet and split into train and test sets
+    evidence, labels = load_data(sys.argv[1])
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE
+    )
+
+    # Train model and make predictions
+    model = train_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    sensitivity, specificity = evaluate(y_test, predictions)
+
+    # Print results
+    print(f"Correct: {(y_test == predictions).sum()}")
+    print(f"Incorrect: {(y_test != predictions).sum()}")
+    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
+    print(f"True Negative Rate: {100 * specificity:.2f}%")
+
+
+def load_data(filename):
+    """
+    Load shopping data from a CSV file `filename` and convert into a list of
+    evidence lists and a list of labels. Return a tuple (evidence, labels).
+
+    evidence should be a list of lists, where each list contains the
+    following values, in order:
+        - Administrative, an integer
+        - Administrative_Duration, a floating point number
+        - Informational, an integer
+        - Informational_Duration, a floating point number
+        - ProductRelated, an integer
+        - ProductRelated_Duration, a floating point number
+        - BounceRates, a floating point number
+        - ExitRates, a floating point number
+        - PageValues, a floating point number
+        - SpecialDay, a floating point number
+        - Month, an index from 0 (January) to 11 (December)
+        - OperatingSystems, an integer
+        - Browser, an integer
+        - Region, an integer
+        - TrafficType, an integer
+        - VisitorType, an integer 0 (not returning) or 1 (returning)
+        - Weekend, an integer 0 (if false) or 1 (if true)
+
+    labels should be the corresponding list of labels, where each label
+    is 1 if Revenue is true, and 0 otherwise.
+    """
+    evidence = []
+    labels = []
+
+    # List of month abbreviations for conversion
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    # Open the CSV file and read data
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+
+        # Process each row in the CSV file
+        for row in reader:
+            # Append labels based on the last column ("TRUE" -> 1, else -> 0)
+            if row[-1] == "TRUE":
+                labels.append(1)
+            else:
+                labels.append(0)
+
+            # Append evidence list with converted values from the row
+            evidence.append([
+                int(row[0]),
+                float(row[1]),
+                int(row[2]),
+                float(row[3]),
+                int(row[4]),
+                float(row[5]),
+                float(row[6]),
+                float(row[7]),
+                float(row[8]),
+                float(row[9]),
+                months.index(row[10]),
+                # Convert month to index
+                int(row[11]),
+                int(row[12]),
+                int(row[13]),
+                int(row[14]),
+                1 if row[15] == "Returning_Visitor" else 0,                    
+                1 if row[16] == "TRUE" else 0
+                ])
+        return evidence, labels
+
+
+def train_model(evidence, labels):
+    """
+    Given a list of evidence lists and a list of labels, return a
+    fitted k-nearest neighbor model (k=1) trained on the data.
+    """
+    # Create a k-nearest neighbor classifier with k=1
+    neigh = KNeighborsClassifier(n_neighbors=1)
+
+    # Train the model using the provided evidence and labels
+    neigh.fit(evidence, labels)
+    # Return the trained model
+    return neigh
+
+
+def evaluate(labels, predictions):
+    """
+    Given a list of actual labels and a list of predicted labels,
+    return a tuple (sensitivity, specificity).
+
+    Assume each label is either a 1 (positive) or 0 (negative).
+
+    `sensitivity` should be a floating-point value from 0 to 1
+    representing the "true positive rate": the proportion of
+    actual positive labels that were accurately identified.
+
+    `specificity` should be a floating-point value from 0 to 1
+    representing the "true negative rate": the proportion of
+    actual negative labels that were accurately identified.
+    """
+    true_positives = 0
+    actual_positives = 0
+    true_negatives = 0
+    actual_negatives = 0
+
+    # Iterate through each index in the labels and predictions lists
+    for index in range(len(labels)):
+        # Check if the actual label is positive (1)
+        if labels[index] == 1:
+            actual_positives += 1
+            # Check if the prediction is also positive (true positive)
+            if predictions[index] == 1:
+                true_positives += 1
+
+        else:
+            # The actual label is negative (0)
+            actual_negatives += 1
+            # Check if the prediction is also negative (true negative)
+            if predictions[index] == 0:
+                true_negatives += 1
+        
+    # Calculate sensitivity and specificity
+    sensitivity = true_positives / actual_positives
+    specificity = true_negatives / actual_negatives
+
+    # Return the calculated sensitivity and specificity
+    return sensitivity, specificity
+
+if __name__ == "__main__":
+    main()
